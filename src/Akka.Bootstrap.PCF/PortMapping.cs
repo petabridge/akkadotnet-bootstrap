24c5d7cf212d58cbd0c7c1b76abafe3f39118c1e
Aaron Stannard <aaron@petabridge.com>
2018-04-11 13:39:42 -0500

    [WIP] Pivotal Cloud Foundry support (#5)
    * added Akka.Bootstrap.PCF projects
    
    * added PCF serializer for VCAP_APPLICATION
    
    * added mechanism for parsing all PCF environment variables into usable classes
    
    * testing PcfEnvironment class
    
    * made PcfEnvironment.Instance public

---
+++ src/Akka.Bootstrap.PCF/PortMapping.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PortMapping.cs" company="Petabridge, LLC">
+//      Copyright (C) 2018 - 2018 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Akka.Bootstrap.PCF
+{
+    /// <summary>
+    ///     Expresses a mapping between an internal and external port
+    /// </summary>
+    public sealed class PortMapping
+    {
+        public PortMapping(int external, int @internal)
+        {
+            External = external;
+            Internal = @internal;
+        }
+
+        /// <summary>
+        ///     The external port visible to remote parties.
+        /// </summary>
+        public int External { get; }
+
+        /// <summary>
+        ///     The internal port used internally.
+        /// </summary>
+        public int Internal { get; }
+
+        public override string ToString()
+        {
+            return $"[External: {External}, Internal: {Internal}]";
+        }
+    }
+}
---
+++ src/Akka.Bootstrap.PCF/Serialization/JsonVcapApplication.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="JsonVcapApplication.cs" company="Petabridge, LLC">
+//      Copyright (C) 2018 - 2018 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Akka.Bootstrap.PCF.Serialization
+{
+    /// <summary>
+    ///     INTERNAL API.
+    ///     JSON representation of the application resource limits in PCF.
+    /// </summary>
+    public class JsonLimits
+    {
+        public int disk { get; set; }
+        public int fds { get; set; }
+        public int mem { get; set; }
+    }
+
+    /// <summary>
+    ///     INTERNAL API.
+    ///     JSON representation of the VCAP_APPLICATION environment variable.
+    /// </summary>
+    /// <remarks>
+    ///     See https://docs.run.pivotal.io/devguide/deploy-apps/environment-variable.html for more details.
+    /// </remarks>
+    public class JsonVcapApplication
+    {
+        public string application_id { get; set; }
+        public string application_name { get; set; }
+        public List<string> application_uris { get; set; }
+        public string application_version { get; set; }
+        public string cf_api { get; set; }
+        public JsonLimits limits { get; set; }
+        public string name { get; set; }
+        public string space_id { get; set; }
+        public string space_name { get; set; }
+        public List<string> uris { get; set; }
+        public object users { get; set; }
+        public string version { get; set; }
+    }
+}
