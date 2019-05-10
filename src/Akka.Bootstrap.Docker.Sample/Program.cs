24dd7a3500b37c1f33a92d6319760b2f17632d70
Aaron Stannard <aaron@petabridge.com>
2019-05-10 14:27:44 -0500

    working on adding an End2End docker sample (#32)
    * working on adding an End2End docker sample
    
    * deleted duplicate .CSPROJ
    
    * renamed sample folder
    
    * upgrading to .NET Core 2.2
    
    * fixed .NET install scripts for 2.2
    
    * added Dockerfile for linux
    
    * revert .NET Core 2.2 SDK hijinks
    
    * adding docker buildscript
    
    * adding Docker build scripts
    
    * fixed param checking
    
    * completed setup scripts
    
    * update Microsoft.SourceLink.Github

---
+++ src/Akka.Bootstrap.Docker.Sample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Event;
+using Akka.Routing;
+
+namespace Akka.Bootstrap.Docker.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Debugger.IsAttached) // If we're launching running from visual studio, set CLUSTER_IP
+            {
+                Environment.SetEnvironmentVariable("CLUSTER_IP", "localhost");
+            }
+
+            var config = ConfigurationFactory.ParseString(File.ReadAllText("app.conf"))
+                .BootstrapFromDocker(); // forces all Docker environment variable substitution
+            var actorSystem = ActorSystem.Create("DockerBootstrap", config);
+
+            var echo = actorSystem.ActorOf(Props.Create(() => new EchoActor()), "echo");
+            var router = actorSystem.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "router");
+
+
+
+            /*
+             * Wait until we've joined the cluster before we begin messaging any other nodes.
+             */
+            var count = 0;
+            Cluster.Cluster.Get(actorSystem).RegisterOnMemberUp(() =>
+            {
+                actorSystem.Scheduler.Advanced.ScheduleRepeatedly(TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(1), () =>
+                {
+                    router.Tell(count++);
+                });
+            });
+
+            // block until the ActorSystem is terminated (try "cluster leave" using Petabridge.Cmd https://cmd.petabridge.com/articles/commands/cluster-commands.html)
+            actorSystem.WhenTerminated.Wait();
+        }
+    }
+
+    public sealed class EchoActor : ReceiveActor
+    {
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+        public EchoActor()
+        {
+            ReceiveAny(_ =>
+            {
+                _log.Info("Received {0} from {1}", _,Sender);
+            });
+        }
+    }
+}
