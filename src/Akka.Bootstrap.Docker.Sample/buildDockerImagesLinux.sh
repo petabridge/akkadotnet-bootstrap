a9e85df3c3391d4f54f8ee441adcb4cc0d64e0fe
Aaron Stannard <aaron@petabridge.com>
2019-05-10 14:16:05 -0500

    fixed param checking

--- src/Akka.Bootstrap.Docker.Sample/buildDockerImagesLinux.sh
+++ src/Akka.Bootstrap.Docker.Sample/buildDockerImagesLinux.sh
@@ -7,16 +7,19 @@
 IMAGE_VERSION=$1
 IMAGE_NAME=$2
 
-if [-z $IMAGE_VERSION]; then
+if [ -z $IMAGE_VERSION ]; then
 	echo `date`" - Missing mandatory argument: Docker image version."
 	echo `date`" - Usage: ./buildDockerImagesLinux.sh [imageVersion] [imageName]"
+	exit 1
 fi
 
-if [-z $IMAGE_NAME ]; then
-	IMAGE_NAME = "akka.docker.boostrap"
-	echo "Using default Docker image name [$IMAGE_NAME]"
+if [ -z $IMAGE_NAME ]; then
+	IMAGE_NAME="akka.docker.boostrap"
+	echo `date`"Using default Docker image name [$IMAGE_NAME]"
 fi
 
 echo "Building project..."
 exec dotnet publish -c Release
 
+LINUX_IMAGE="$IMAGE_NAME:$IMAGE_VERSION-linux"
+LINUX_IMAGE_LATEST="$IMAGE_NAME:latest-linux"
